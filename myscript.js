496a12f0665fcee7476825043a98d28cf7e0b54c
csforumwa <82461846+csforumwa@users.noreply.github.com>
2021-08-13 11:31:08 +0000

    Deploying to gh-pages from master @ 38cd8b53943d0479737e84264b9533a12e8ee999 🚀

--- myscript.js
+++ myscript.js
@@ -1,1 +1,21 @@
 WA.chat.sendChatMessage('Hello world', 'Mr Robot');
+
+
+var helloWorldPopup;
+
+// Open the popup when we enter a given zone
+helloWorldPopup = WA.room.onEnterZone('myZone', () => {
+    WA.ui.openPopup("popupRectangle", 'Hello world!', [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }]);
+});
+
+// Close the popup when we leave the zone.
+WA.room.onLeaveZone('myZone', () => {
+    helloWorldPopup.close();
+});
