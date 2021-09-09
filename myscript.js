2936ba2173b6eab92ed54052e639da68c3daa510
csforumwa <cybersecurity@sick.de>
2021-09-09 15:22:11 +0200

    myscript 4 popup

--- myscript.js
+++ myscript.js
@@ -1,36 +1,15 @@
 //WA.chat.sendChatMessage('Hello world', 'Mr Robot');
-
-var zone0WelcomeName = "popUp0Welcome";
-var ispopUp0WelcomeSeen = "no";
-// Test if the player already seen this popUp or not
-WA.onEnterZone(zone0WelcomeName, () => {
-if(ispopUp0WelcomeSeen == "no"){
-WA.disablePlayerControls();
-ispopUp0WelcomeSeen = "yes";
-currentPopup =  WA.openPopup("popUp0Welcome","Welcome to this WorkAdventure map.",[
-{
-label: "OK",
-className: "popUpElement",
-callback: (popup => {
-// WA.openCoWebSite(urlTeleport);
-// isCoWebSiteOpened = true;
-WA.restorePlayerControls();
-closePopUp();
-})
-}]);
-}
-})
-WA.onLeaveZone(zone0WelcomeName, closePopUp)
-
-
-
-
-
-/*
-
+ 
 WA.ui.openPopup(targetObject:"myPopUp", message: "Helas!", buttons: ButtonDescriptor[]): Popup
 
 var helloWorldPopup;
+
+class Popup {
+    /**
+     * Closes the popup
+     */
+    close() {};
+}
 
 // Open the popup when we enter a given zone
 helloWorldPopup = WA.room.onEnterZone('myZone', () => {
@@ -49,4 +28,3 @@ WA.room.onLeaveZone('myZone', () => {
     helloWorldPopup.close();
 });
 
-*/
